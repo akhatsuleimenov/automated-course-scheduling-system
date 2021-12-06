4d1c38fa4c70649ec8c99920d66ce01759b95766
Akhat <akhatsuleimenov@gmail.com>
2021-12-06 11:20:04 +0400

    Fixed scheduling for fixed and unfixed courses

--- Scraping + Scheduling/scraping.py
+++ Scraping + Scheduling/scraping.py
@@ -138,17 +138,22 @@ def trimTitle(title):
         title = title[1:]
     return title
 
-def modifySelectedCourses(selected_courses):
+def modifySelectedCourses(selected_courses, index):
     for i in range(len(selected_courses)):
         selected_courses[i] = (selected_courses[i], 'LEC')
     for i in range(len(selected_courses)):
-        for course in id_dict[selected_courses[i][0]]:
+        id_ = selected_courses[i][0].id if i < index else selected_courses[i][0]
+        for course in id_dict[id_]:
             if trimTitle(course.title) in extra:
                 selected_courses.append((course.id, 'RCT'))
                 break
 
-def scheduling(selected_courses):
-    calendar = backtracking([], [], selected_courses, 0, [])
+def scheduling(selected_courses, index):
+    schedule = []
+    if index:
+        for i in range(index):
+            schedule.append(selected_courses[i][0])
+    calendar = backtracking([], schedule, selected_courses, index)
     ''' to be deleted '''
     if not calendar:
         print("No possible schedule")
@@ -161,9 +166,9 @@ def scheduling(selected_courses):
     ''' to be deleted '''
     return calendar
 
-def backtracking(calendar, schedule, selected_courses, index, messages):
+def backtracking(calendar, schedule, selected_courses, index):
     if index == len(selected_courses):
-        calendar.append((schedule[:], messages[:]))
+        calendar.append(schedule[:])
         return calendar
 
     # check if time with days are possible in the schedule
@@ -177,8 +182,8 @@ def backtracking(calendar, schedule, selected_courses, index, messages):
         days = set(course.days)
         for selected_course in schedule:
             days_intersection = days.intersection(selected_course.days)
+            added_start, added_end = selected_course.start_date, selected_course.end_date
             for day in days_intersection:
-                added_start, added_end = selected_course.start_date, selected_course.end_date
                 if not (added_start > end or added_end < start):
                     possible = False
                     break
@@ -186,18 +191,51 @@ def backtracking(calendar, schedule, selected_courses, index, messages):
                 break
         if possible:
             schedule.append(course)
-            if course.status == 'Wait List': messages.append("{} is waitlisted. You are going to be number {} in the queue. Be aware of that.".format(course.name, course.waitlist_count))
-            backtracking(calendar, schedule, selected_courses, index + 1, messages)
+            backtracking(calendar, schedule, selected_courses, index + 1)
             schedule.pop()
-            if course.status == 'Wait List': messages.pop()
     return calendar
+
+def checkFixedCourses(fixed_courses):
+    for i in range(len(fixed_courses) - 1):
+        start, end = fixed_courses[i].start_date, fixed_courses[i].end_date
+        for j in range(i + 1, len(fixed_courses)):
+            days_intersection = set(fixed_courses[i].days).intersection(fixed_courses[j].days)
+            added_start, added_end = fixed_courses[j].start_date, fixed_courses[j].end_date
+            for day in days_intersection:
+                if not (added_start > end or added_end < start):
+                    return False
+    return True
+
+
+def correctSelectedCourses(selected_courses):
+    selected_count = 0
+    fixed_selected_courses = []
+    for course_id, course_term in selected_courses:
+        if not course_term:
+            fixed_selected_courses.append(course_id)
+        else:
+            selected_count += 1
+            print(course_term)
+            for c in id_dict[course_id]:
+                if c.title == course_term:
+                    fixed_selected_courses.insert(0, c)
+                    break
+    return (fixed_selected_courses, selected_count)
 
 if __name__ == '__main__':
     parsing()
     data = savingJSONtoList('courses.json', 'r')
     sortDict()
-    selected_courses = ['MATHUH1000A234160', 'ARABLUH1120160332', 'ARABLUH2120204522', 'ARTHUH2128232572', 'AWUH1118236369']
-    modifySelectedCourses(selected_courses)
-    calendar = scheduling(selected_courses)
-    toJSON('schedule.json', 'w', calendar)
-    JSONToExcel("schedule.json", "calendar.xlsx")
+
+    # selected_courses = savingJSONtoList("selectedCourses.json", 'r')
+    selected_courses = [('MATHUH1000A234160', '001-SEM (18450)'), ('ARABLUH1120160332', 0), ('ARABLUH2120204522', 0), ('ARTHUH2128232572', 0), ('AWUH1118236369', 0)]
+    fixed_selected_courses, selected_count = correctSelectedCourses(selected_courses)
+    if checkFixedCourses(fixed_selected_courses[:selected_count]):
+        
+        modifySelectedCourses(fixed_selected_courses, selected_count)
+        calendar = scheduling(fixed_selected_courses, selected_count)
+        
+        toJSON('schedule.json', 'w', calendar)
+        JSONToExcel("schedule.json", "calendar.xlsx")
+    else:
+        print("No possible schedule")
