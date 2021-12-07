9255b086f2fe5b10af0eb1f8bb0aabef96472055
Akhat <akhatsuleimenov@gmail.com>
2021-12-07 12:24:37 +0400

    Moved all py files into py_files folder

---
+++ backend/py_files/constants.py
@@ -0,0 +1,19 @@
+# parsed courses from Albert
+FOLDER_HTML = 'html_files/'
+COURSES_HTML = FOLDER_HTML + 'data.html'
+
+# json files needed to use at the website
+FOLDER_JSON = 'json_files/'
+CALENDAR_JSON = FOLDER_JSON + 'calendar.json'
+COURSES_JSON = FOLDER_JSON + 'courses.json'
+COURSES_DICT_JSON = FOLDER_JSON + 'courses_dict.json'
+SELECTED_JSON = FOLDER_JSON + 'selected.json'
+SUBCLASSES_JSON = FOLDER_JSON + 'subclasses.json'
+
+# excel files useful for debugging
+FOLDER_EXCEL = 'excel_files/'
+CALENDAR_EXCEL = FOLDER_EXCEL + 'calendar.xlsx'
+COURSES_EXCEL = FOLDER_EXCEL + 'courses.xlsx'
+
+# extra courses that need to be added to the schedule
+EXTRA = {'LAB', 'RCT', 'WKS'}
