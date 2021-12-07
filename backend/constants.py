38da17d5d1406d9db82e087a90b3993478a9cd2a
Akhat <akhatsuleimenov@gmail.com>
2021-12-07 11:42:03 +0400

    Added new constant and sorted by alphabetics

--- backend/constants.py
+++ backend/constants.py
@@ -2,12 +2,15 @@
 COURSES_HTML = 'data.html'
 
 # json files needed to use at the website
+CALENDAR_JSON = 'calendar.json'
 COURSES_JSON = 'courses.json'
 COURSES_DICT_JSON = 'courses_dict.json'
 SELECTED_JSON = 'selected.json'
 SUBCLASSES_JSON = 'subclasses.json'
-CALENDAR_JSON = 'calendar.json'
 
 # excel files useful for debugging
-COURSES_EXCEL = 'courses.xlsx'
 CALENDAR_EXCEL = 'calendar.xlsx'
+COURSES_EXCEL = 'courses.xlsx'
+
+# extra courses that need to be added to the schedule
+EXTRA = {'LAB', 'RCT', 'WKS'}
