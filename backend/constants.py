3f79324fc83cd2f25d555a8d60212ccb51cd6571
Akhat <akhatsuleimenov@gmail.com>
2021-12-07 12:08:50 +0400

    Added seperate folder variables for each type of file

--- backend/constants.py
+++ backend/constants.py
@@ -1,16 +1,19 @@
 # parsed courses from Albert
-COURSES_HTML = 'data.html'
+FOLDER_HTML = 'html_files/'
+COURSES_HTML = FOLDER_HTML + 'data.html'
 
 # json files needed to use at the website
-CALENDAR_JSON = 'calendar.json'
-COURSES_JSON = 'courses.json'
-COURSES_DICT_JSON = 'courses_dict.json'
-SELECTED_JSON = 'selected.json'
-SUBCLASSES_JSON = 'subclasses.json'
+FOLDER_JSON = 'json_files/'
+CALENDAR_JSON = FOLDER_JSON + 'calendar.json'
+COURSES_JSON = FOLDER_JSON + 'courses.json'
+COURSES_DICT_JSON = FOLDER_JSON + 'courses_dict.json'
+SELECTED_JSON = FOLDER_JSON + 'selected.json'
+SUBCLASSES_JSON = FOLDER_JSON + 'subclasses.json'
 
 # excel files useful for debugging
-CALENDAR_EXCEL = 'calendar.xlsx'
-COURSES_EXCEL = 'courses.xlsx'
+FOLDER_EXCEL = 'excel_files/'
+CALENDAR_EXCEL = FOLDER_EXCEL + 'calendar.xlsx'
+COURSES_EXCEL = FOLDER_EXCEL + 'courses.xlsx'
 
 # extra courses that need to be added to the schedule
 EXTRA = {'LAB', 'RCT', 'WKS'}
