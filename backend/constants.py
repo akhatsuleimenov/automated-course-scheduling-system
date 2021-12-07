78481a32a091a659640ff0db9bfe38bf39507a08
Akhat <akhatsuleimenov@gmail.com>
2021-12-07 10:47:06 +0400

    Seperated constant and class into seperate files. Created a makefile

---
+++ backend/constants.py
@@ -0,0 +1,13 @@
+# parsed courses from Albert
+COURSES_HTML = 'data.html'
+
+# json files needed to use at the website
+COURSES_JSON = 'courses.json'
+COURSES_DICT_JSON = 'courses_dict.json'
+SELECTED_JSON = 'selected.json'
+SUBCLASSES_JSON = 'subclasses.json'
+CALENDAR_JSON = 'calendar.json'
+
+# excel files useful for debugging
+COURSES_EXCEL = 'courses.xlsx'
+CALENDAR_EXCEL = 'calendar.xlsx'
