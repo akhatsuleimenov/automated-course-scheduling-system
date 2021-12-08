82b3ce0ee07ceb3b0dde678fcba42ba2f57f7408
Akhat <akhatsuleimenov@gmail.com>
2021-12-08 18:25:11 +0400

    Deleted old coding files

--- backend/py_files/to_excel.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-
-def json_to_excel(infile, outfile):
-    '''Convert json file into excel'''
-    pd.read_json(infile).to_excel(outfile)
-
-if __name__ == '__main__':
-    json_to_excel(sys.argv[1], sys.argv[2])
