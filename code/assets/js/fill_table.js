a7f205d556ae3e1fb5699f38eaff5462528ab646
madinayeleukina <>
2021-12-08 19:05:27 +0400

    added comments

--- code/assets/js/fill_table.js
+++ code/assets/js/fill_table.js
@@ -1,27 +1,148 @@
 "use strict";
+//fetch schedules list
 async function fetchJSON() {
-  const response = await fetch("/schedule.json");
+  const response = await fetch("https://raw.githubusercontent.com/akhatsuleimenov/automated-course-scheduling-system/main/code/backend/json_files/calendar.json");
   const schedules = await response.json();
   return schedules;
 }
 fetchJSON().then(schedules => {
+  //store schedules number
   max_len = schedules.length;
+
+  //display alert message if schedules list is empty
+  if (!max_len) {
+    alert("Valid schedules cannot be generated");
+    $("buttons").addClass("hide");
+  } 
+
+  //update shown schedule number
   if (max_len>0) 
     document.getElementById("schedule-number").innerHTML = "" + (current_schedule+1) + "/" + max_len;
-  else 
+  else {
     document.getElementById("schedule-number").innerHTML = "" + current_schedule + "/" + max_len;
+  }
 
-
+  //create each schedule in html file
   for (var k = 0; k < schedules.length; k++) {
-    var schedule = schedules[k][0];
+    var schedule = schedules[k];
     
-    //SCHEDULE
+    //create schedule div
     var schedule_div = document.createElement("div");
     schedule_div.className = "cd-schedule margin-top-sm margin-bottom-sm js-cd-schedule";
     schedule_div.className += " schedule-"+k;
     if (k>0) schedule_div.className += " hide";
 
-    //TIMELINE
+    //create timeline
+    createTimeline(schedule_div);
+
+    //create courses
+    var events_div = document.createElement("div");
+    events_div.className = "cd-schedule__events";
+    events_div.id = "schedule";
+    var events_ul = document.createElement("ul");
+
+    //split schedule courses into weekdays
+    let sun = new Array, mon = new Array, tue = new Array, wed = new Array, thu = new Array;
+    for (var i=0; i < schedule.length; i++) {
+      schedule[i].event_id = i;
+      if (schedule[i]["status"]=="Wait List") schedule[i].event_id = -1;
+      if (schedule[i]["days"].includes("U")) sun.push(schedule[i]);
+      if (schedule[i]["days"].includes("M")) mon.push(schedule[i]);
+      if (schedule[i]["days"].includes("T")) tue.push(schedule[i]);
+      if (schedule[i]["days"].includes("W")) wed.push(schedule[i]);
+      if (schedule[i]["days"].includes("R")) thu.push(schedule[i]);
+    }
+    var weekdays = [sun,mon,tue,wed,thu];
+    var weekdays_names = ["sun","mon","tue","wed","thu"];
+    var weekdays_full_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+
+    //create courses for each weekday
+    for (var weekday = 0; weekday < weekdays.length; weekday++) {
+      var events_li = document.createElement("li");
+      events_li.className = "cd-schedule__group";
+      //display weekday title
+      var events_topinfo_div = document.createElement("div");
+      events_topinfo_div.className = "cd-schedule__top-info";
+      var events_topinfo_span = document.createElement("span");
+      events_topinfo_span.innerHTML = weekdays_full_names[weekday];
+      events_topinfo_div.appendChild(events_topinfo_span);
+      events_li.appendChild(events_topinfo_div);
+      var events_weekday_ul = document.createElement("ul");
+      events_weekday_ul.id = "ul-"+weekdays_names[weekday];
+
+      var slotHeight = 30;
+      var timelineStart = getStringToTimestamp("9:00");
+      var timelineUnitDuration = 30;
+      var cur = weekdays[weekday];
+
+      //create courses of certain weekday
+      for (var i = 0; i < cur.length; i++) {
+        //calculate course position
+        var start = getNumberToTimestamp(cur[i]["start_date"]);
+        var duration = getNumberToTimestamp(cur[i]["end_date"]) - start;
+        var courseTop = slotHeight*(start - timelineStart)/timelineUnitDuration;
+        var courseHeight = slotHeight*duration/timelineUnitDuration;
+
+        //create course element
+        var name = document.createElement("em");
+        name.className = "cd-schedule__name ";
+        name.innerHTML = cur[i]["name"].split(" - ")[1];
+        var text = document.createElement("a");
+        text.dataset.start = getNumberToStringTime(cur[i]["start_date"]);
+        text.dataset.end = getNumberToStringTime(cur[i]["end_date"]);
+        text.dataset.event = "event-"+cur[i]["event_id"];
+        text.dataset.prof = cur[i]["instructor"];
+        text.dataset.schedule = k;
+        text.appendChild(name);
+        var tag = document.createElement("li");
+        tag.className = "cd-schedule__event";
+        tag.appendChild(text);
+
+        //position course element
+        tag.setAttribute('style', 'top: '+(courseTop-1)+'px; height: '+(courseHeight +1)+'px');
+
+        //add course element into parent node
+        events_weekday_ul.appendChild(tag);
+      }
+      events_li.appendChild(events_topinfo_div);
+      events_li.appendChild(events_weekday_ul);
+      events_ul.appendChild(events_li);
+    }
+
+    events_div.appendChild(events_ul);
+    schedule_div.appendChild(events_div);
+
+    //add schedule div to html file
+    document.getElementById("schedules-slider").appendChild(schedule_div);
+  }
+
+  //append main script to html
+  var src = "assets/js/main.js";
+  $('<script>').attr('src', src).appendTo('body');
+
+  //convert time in string(hh:mm) into timestamp(total minutes)
+  function getStringToTimestamp(time) {
+    var timeArray = (time+"").split(':');
+    var timeStamp = parseInt(timeArray[0])*60 + parseInt(timeArray[1]);
+    return timeStamp;
+  }
+
+  //convert time encoded by number([hh].[mm/60]) into timestamp(total minutes)
+  function getNumberToTimestamp(time) {
+    var timeStamp = Math.trunc(time)*60 + Math.round((time%1)*60);
+    return timeStamp;
+  }
+
+  //convert time encoded by number([hh].[mm/60]) into string(hh:mm)
+  function getNumberToStringTime(time) {
+    var hour = "" + Math.trunc(time);
+    var min = "" + Math.round((time%1)*60);
+    while (min.length<2) min = "0"+min;
+    return hour + ":" + min;
+  }
+
+  //create timeline
+  function createTimeline(parent) {
     var timeline_div = document.createElement("div");
     timeline_div.className = "cd-schedule__timeline";
     var timeline_ul = document.createElement("ul");
@@ -39,119 +160,11 @@ fetchJSON().then(schedules => {
       }
     }
     timeline_div.appendChild(timeline_ul);
-    schedule_div.appendChild(timeline_div);
-
-    //EVENTS
-    var events_div = document.createElement("div");
-    events_div.className = "cd-schedule__events";
-    events_div.id = "schedule";
-    var events_ul = document.createElement("ul");
-
-    let sun = new Array, mon = new Array, tue = new Array, wed = new Array, thu = new Array;
-    for (var i=0; i < schedule.length; i++) {
-      schedule[i].event_id = i;
-      if (schedule[i]["days"].includes("U")) sun.push(schedule[i]);
-      if (schedule[i]["days"].includes("M")) mon.push(schedule[i]);
-      if (schedule[i]["days"].includes("T")) tue.push(schedule[i]);
-      if (schedule[i]["days"].includes("W")) wed.push(schedule[i]);
-      if (schedule[i]["days"].includes("R")) thu.push(schedule[i]);
-    }
-    var weekdays = [sun,mon,tue,wed,thu];
-    var weekdays_names = ["sun","mon","tue","wed","thu"];
-    var weekdays_full_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
-    // <div class="cd-schedule__events" id="schedule"> DO WE NEED ID??
-    //   <ul>
-    //     <li class="cd-schedule__group">
-    //       <div class="cd-schedule__top-info"><span>Sunday</span></div>
-    //       <ul id="ul-sun">
-    for (var weekday = 0; weekday < weekdays.length; weekday++) {
-      var events_li = document.createElement("li");
-      events_li.className = "cd-schedule__group";
-      var events_topinfo_div = document.createElement("div");
-      events_topinfo_div.className = "cd-schedule__top-info";
-      var events_topinfo_span = document.createElement("span");
-      events_topinfo_span.innerHTML = weekdays_full_names[weekday];
-      events_topinfo_div.appendChild(events_topinfo_span);
-      events_li.appendChild(events_topinfo_div);
-      var events_weekday_ul = document.createElement("ul");
-      events_weekday_ul.id = "ul-"+weekdays_names[weekday];
-
-      var slotHeight = 30;//events_topinfo_div.offsetHeight;
-      //var timelineItems = document.getElementsByClassName('cd-schedule__timeline')[0].getElementsByTagName('li');
-      var timelineStart = getStringToTimestamp("9:00");//getStringToTimestamp(timelineItems[0].textContent);
-      var timelineUnitDuration = 30;//getStringToTimestamp(timelineItems[1].textContent)-timelineStart;
-      var cur = weekdays[weekday];
-
-      for (var i = 0; i < cur.length; i++) {
-        var start = getNumberToTimestamp(cur[i]["start_date"]);
-        var duration = getNumberToTimestamp(cur[i]["end_date"]) - start;
-        var courseTop = slotHeight*(start - timelineStart)/timelineUnitDuration;
-        var courseHeight = slotHeight*duration/timelineUnitDuration;
-
-        //create a course element
-        var name = document.createElement("em");
-        name.className = "cd-schedule__name ";
-        name.innerHTML = cur[i]["name"].split(" - ")[1];
-        var text = document.createElement("a");
-        text.dataset.start = getNumberToStringTime(cur[i]["start_date"]);
-        text.dataset.end = getNumberToStringTime(cur[i]["end_date"]);
-        text.dataset.event = "event-"+cur[i]["event_id"];
-        text.dataset.prof = cur[i]["instructor"];
-        text.dataset.schedule = k;
-        text.appendChild(name);
-        var tag = document.createElement("li");
-        tag.className = "cd-schedule__event";
-        //if (k != current_schedule) tag.className = "cd-schedule__event hide " + "schedule-" + k;//!!!
-        //else tag.className = "cd-schedule__event " + "schedule-" + k;
-        tag.appendChild(text);
-
-        //position a course element
-        tag.setAttribute('style', 'top: '+(courseTop-1)+'px; height: '+(courseHeight +1)+'px');
-
-        //add a course element into html file
-        events_weekday_ul.appendChild(tag);
-      }
-      events_li.appendChild(events_topinfo_div);
-      events_li.appendChild(events_weekday_ul);
-      events_ul.appendChild(events_li);
-    }
-
-    events_div.appendChild(events_ul);
-    schedule_div.appendChild(events_div);
-
-    //COVER LAYER
-    var cover_div = document.createElement("div");
-    cover_div.className = "cd-schedule__cover-layer";
-    schedule_div.appendChild(cover_div);
-
-    //document.body.prepend(schedule_div);
-    document.getElementById("schedules-slider").appendChild(schedule_div);
+    parent.appendChild(timeline_div);
   }
-
-  var src = "assets/js/util.js";
-  $('<script>').attr('src', src).appendTo('body');
-  var src = "assets/js/main.js";
-  $('<script>').attr('src', src).appendTo('body');
-
-
-  function getStringToTimestamp(time) {
-    //accepts hh:mm format - convert hh:mm to timestamp
-    var timeArray = (time+"").split(':');
-    var timeStamp = parseInt(timeArray[0])*60 + parseInt(timeArray[1]);
-    return timeStamp;
-  }
-
-  function getNumberToTimestamp(time) {
-    //accepts hh.mm/60 format - convert hh.mm/60 to timestamp
-    var timeStamp = Math.trunc(time)*60 + Math.round((time%1)*60);
-    return timeStamp;
-  }
-
-  function getNumberToStringTime(time) {
-    //accepts hh.mm/60 format - convert hh.mm/60 to hh:mm
-    var hour = "" + Math.trunc(time);
-    var min = "" + Math.round((time%1)*60);
-    while (min.length<2) min = "0"+min;
-    return hour + ":" + min;
-  }
+})
+.catch((error) => {
+  $("buttons").addClass("hide");
+  alert("Ooops, something went wrong:( Please, try again later!");
+  console.log(error);
 });
--- code/assets/js/main.js
+++ code/assets/js/main.js
@@ -1,96 +1,14 @@
-// (function() {
-// 	// Schedule Template - by CodyHouse.co
-// 	function ScheduleTemplate( element ) {
-// 		this.element = element;
-// 		this.timelineItems = this.element.getElementsByClassName('cd-schedule__timeline')[0].getElementsByTagName('li');
-// 		this.timelineStart = getScheduleTimestamp(this.timelineItems[0].textContent);
-// 		this.timelineUnitDuration = getScheduleTimestamp(this.timelineItems[1].textContent) - getScheduleTimestamp(this.timelineItems[0].textContent);
-// 		this.topInfoElement = this.element.getElementsByClassName('cd-schedule__top-info')[0];
-// 		this.singleEvents = this.element.getElementsByClassName('cd-schedule__event');
-// 		this.coverLayer = this.element.getElementsByClassName('cd-schedule__cover-layer')[0];
-// 		this.initSchedule();
-// 	};
-
-// 	ScheduleTemplate.prototype.initSchedule = function() {
-// 		this.scheduleReset();
-// 	};
-
-// 	ScheduleTemplate.prototype.scheduleReset = function() {
-// 		// according to the mq value, init the style of the template
-// 		var mq = this.mq(),
-// 			loaded = Util.hasClass(this.element, 'js-schedule-loaded');
-// 		if( mq == 'desktop' && !loaded ) {
-// 			Util.addClass(this.element, 'js-schedule-loaded');
-// 			this.placeEvents();
-// 		} else if( mq == 'mobile' && loaded) {
-// 			//in this case you are on a mobile version (first load or resize from desktop)
-// 			Util.removeClass(this.element, 'cd-schedule--loading js-schedule-loaded');
-// 			this.resetEventsStyle();
-// 		} else {
-// 			Util.removeClass(this.element, 'cd-schedule--loading');
-// 		}
-// 	};
-
-// 	ScheduleTemplate.prototype.resetEventsStyle = function() {
-// 		// remove js style applied to the single events
-// 		for(var i = 0; i < this.singleEvents.length; i++) {
-// 			this.singleEvents[i].removeAttribute('style');
-// 		}
-// 	};
-
-// 	ScheduleTemplate.prototype.placeEvents = function() {
-// 		Util.removeClass(this.element, 'cd-schedule--loading');
-// 	};
-
-// 	ScheduleTemplate.prototype.mq = function(){
-// 		//get MQ value ('desktop' or 'mobile') 
-// 		var self = this;
-// 		return window.getComputedStyle(this.element, '::before').getPropertyValue('content').replace(/'|"/g, "");
-// 	};
-
-// 	function getScheduleTimestamp(time) {
-// 		//accepts hh:mm format - convert hh:mm to timestamp
-// 		time = time.replace(/ /g,'');
-// 		var timeArray = time.split(':');
-// 		var timeStamp = parseInt(timeArray[0])*60 + parseInt(timeArray[1]);
-// 		return timeStamp;
-// 	};
-
-// 	var scheduleTemplate = document.getElementsByClassName('js-cd-schedule'),	
-// 		scheduleTemplateArray = [],
-// 		resizing = false;
-
-// 	if( scheduleTemplate.length > 0 ) { // init ScheduleTemplate objects
-// 		for( var i = 0; i < scheduleTemplate.length; i++) {
-// 			(function(i){
-// 				scheduleTemplateArray.push(new ScheduleTemplate(scheduleTemplate[i]));
-// 			})(i);
-// 		}
-
-// 		window.addEventListener('resize', function(event) { 
-// 			// on resize - update events position and modal position (if open)
-// 			if( !resizing ) {
-// 				resizing = true;
-// 			}
-// 		});
-
-// 		function checkResize(){
-// 			for(var i = 0; i < scheduleTemplateArray.length; i++) {
-// 				scheduleTemplateArray[i].scheduleReset();
-// 			}
-// 			resizing = false;
-// 		};
-// 	}
-// }());
-
+//hide schedules with given id
 function hide(id) {
  	$(".schedule-"+id).addClass("hide");
 };
 
+//show schedules with given id
 function show(id) {
  	$(".schedule-"+id).removeClass("hide");
 };
 
+//update shown schedule number
 function scheduleNumber_update() {
   if (max_len>0) 
     document.getElementById("schedule-number").innerHTML = "" + (current_schedule+1) + "/" + max_len;
@@ -98,23 +16,21 @@ function scheduleNumber_update() {
     document.getElementById("schedule-number").innerHTML = "" + current_schedule + "/" + max_len;
 }
 
+//show previous schedule
 prev_schedule = function(event) {
-  if (current_schedule>0) {
-  	hide(current_schedule);
-  	current_schedule--;
-  	show(current_schedule);
-  }
-  
+	hide(current_schedule);
+  //if negative, show last
+	current_schedule = ((current_schedule - 1) + max_len) % max_len;
+	show(current_schedule);
   scheduleNumber_update();
 };
 
+//show next schedule
 next_schedule = function(event) {
-  if (current_schedule<max_len-1) {
-  	hide(current_schedule);
-  	current_schedule++;
-  	show(current_schedule);
-  }
-
+  hide(current_schedule);
+  //if exceeds max_len show first
+  current_schedule = (current_schedule + 1) % max_len;
+  show(current_schedule);
   scheduleNumber_update();
 };
 
